35c76398bb43e699ac61077184f505f5d4212155
m15035689636 <15035689636@163.com>
2019-05-05 20:37:10 +0800

    bilibili头部完成 鬼畜内容部分完成 已合并footer部分

---
+++ wedsite/guichu/js/dinu.js
@@ -0,0 +1,18 @@
+$('.q1').mouseenter(function () {
+    $(this).find('span').addClass('active')
+})
+$('.q1').mouseleave(function () {
+    $(this).find('span').removeClass('active')
+})
+$('.q2').mouseenter(function () {
+    $(this).find('span').addClass('active')
+})
+$('.q2').mouseleave(function () {
+    $(this).find('span').removeClass('active')
+})
+$('.q3').mouseenter(function () {
+    $(this).find('span').addClass('active')
+})
+$('.q3').mouseleave(function () {
+    $(this).find('span').removeClass('active')
+})
